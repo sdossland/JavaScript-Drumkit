7314271e66c28e3b08ce8291971a458d370b1f8c
Sophia Dossland <sophia.dossland@gmail.com>
2017-02-23 23:06:51 -0800

    end the keydown transition

--- logic.js
+++ logic.js
@@ -1,24 +1,26 @@
 /**
  * Created by sophia on 2/23/17.
  */
-function playAudio(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  if (!audio) return; //if invalid key is selected, return null
-  audio.currentTime = 0; //rewinds sound immediately to allow for repetition
-  audio.play();
-  key.classList.add('playing');
-}
+document.addEventListener("DOMContentLoaded", function() {
+  function playAudio(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return; //if invalid key is selected, return null
+    audio.currentTime = 0; //rewinds sound immediately to allow for repetition
+    audio.play();
+    key.classList.add('playing');
+  }
 
-function removeTransition(e) {
-  // if (e.propertyName !== 'transform') return; //only need to pick the longest event
-  this.classList.remove('playing'); //'this' refers to the element being called, in this case 'key' [line 21]
-}
+  function removeTransition() {
+    // if (e.propertyName !== 'transform') return; //only need to pick the longest event
+    this.classList.remove('playing'); //'this' refers to the element being called, in this case 'key' [line 21]
+  }
 
-window.addEventListener('keydown', playAudio);
+  window.addEventListener('keydown', playAudio);
 
-const keys = document.querySelectorAll('.key');
+  const keys = document.querySelectorAll('.key');
 //must loop through all of the elements, cannot listen to a whole list
-keys.forEach(function (key) {
-  key.addEventListener('webkitTransitionEnd', removeTransition);
+  keys.forEach(function (key) {
+    key.addEventListener('transitionend', removeTransition);
+  });
 });
